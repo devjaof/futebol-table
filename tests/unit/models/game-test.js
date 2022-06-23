26ed791264f0081ee1cb68f49141f28bbb38dcb6
Joao Francisco <joao.rodrigues@viasoft.com.br>
2022-06-23 11:23:42 -0300

    defining models

---
+++ tests/unit/models/game-test.js
@@ -0,0 +1,13 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'fut-table/tests/helpers';
+
+module('Unit | Model | game', function (hooks) {
+  setupTest(hooks);
+
+  // Replace this with your real tests.
+  test('it exists', function (assert) {
+    let store = this.owner.lookup('service:store');
+    let model = store.createRecord('game', {});
+    assert.ok(model);
+  });
+});
